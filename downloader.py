f327f6fde98d9f3178eda5557837f562fbef0116
MicheleUnibg <michelezanchi94@gmail.com>
2016-03-24 09:12:34 +0100

    Added files via upload

--- downloader.py
+++ downloader.py
@@ -8,8 +8,11 @@ conn=sqlite3.connect("/home/michele/Scrivania/Tesi/Crawler/database.db")
 c=conn.cursor()
 
 #url="http://f.giveawaycrew.com/f.php?p=4&i=com.camelgames.fantasyland&v=1.24.0&h=N0Y4UWdjYlN1K1BVd0VYanRqdTl2Zz09&d=K010RjU3RlVxcEdTT1c5T0hFdHpyQT09"
-c.execute("select titolo,linkDownload from prova2 where scaricato='false' LIMIT 1")
+c.execute("select identificativo,titolo,linkDownload from prova2 where scaricato='false' LIMIT 1")
 ris=c.fetchone()
 testfile = urllib.URLopener()
-testfile.retrieve(ris[1], "App/"+ris[0])
+testfile.retrieve(ris[2], "App/"+ris[1])
+c.execute("UPDATE prova2 set scaricato=? WHERE identificativo=?",('true',int(ris[0]),))
+conn.commit()
+conn.close()
 
